9309eebd20e78419dd1325e8541822cc25ead249
Maša Mastilović <masamastilovic@gmail.com>
2025-08-28 19:31:06 +0200

    feat: toolbar

--- Habit-quest/app/src/main/java/com/example/habitquest/MainActivity.java
+++ Habit-quest/app/src/main/java/com/example/habitquest/MainActivity.java
@@ -3,22 +3,57 @@ package com.example.habitquest;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.app_name); // ← ovde app name
+        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull android.view.MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_categories) {
+            setTitle(R.string.title_categories);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_container, new com.example.habitquest.fragments.CategoryListFragment(), "CAT")
+                    .commit();
+            return true;
+        } else if (id == R.id.action_tasks) {
+            setTitle(R.string.title_tasks);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_container, new com.example.habitquest.fragments.TaskListFragment(), "TASK")
+                    .commit();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
