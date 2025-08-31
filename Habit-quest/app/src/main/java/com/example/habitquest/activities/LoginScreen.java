0bfa046005b54ff3eceacb1976dcc9756fc012d8
Ana Edelinski <aedelinski85@gmail.com>
2025-08-31 16:22:40 +0200

    Simple SignIn UI

---
+++ Habit-quest/app/src/main/java/com/example/habitquest/activities/LoginScreen.java
@@ -0,0 +1,32 @@
+package com.example.habitquest.activities;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.habitquest.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+    }
+}
