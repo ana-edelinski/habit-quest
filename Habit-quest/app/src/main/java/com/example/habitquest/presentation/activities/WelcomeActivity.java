87f316ece84d836f83743d2882af9846309ed774
Ana Edelinski <aedelinski85@gmail.com>
2025-10-04 01:06:58 +0200

    Stay logged in

--- Habit-quest/app/src/main/java/com/example/habitquest/presentation/activities/WelcomeActivity.java
+++ Habit-quest/app/src/main/java/com/example/habitquest/presentation/activities/WelcomeActivity.java
@@ -7,12 +7,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.habitquest.R;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_welcome);
 
         MaterialButton btnSignUp = findViewById(R.id.btnSignUp);
@@ -25,6 +34,5 @@ public class WelcomeActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(v -> {
             startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
         });
-
     }
 }
