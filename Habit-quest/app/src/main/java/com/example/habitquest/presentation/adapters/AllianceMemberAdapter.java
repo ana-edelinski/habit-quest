1a733020738cb0e079bf2e191d43d59527f37b6d
Ana Edelinski <aedelinski85@gmail.com>
2025-10-06 22:51:31 +0200

    Alliance Details UI

--- Habit-quest/app/src/main/java/com/example/habitquest/presentation/adapters/AllianceMemberAdapter.java
+++ Habit-quest/app/src/main/java/com/example/habitquest/presentation/adapters/AllianceMemberAdapter.java
@@ -3,6 +3,7 @@ package com.example.habitquest.presentation.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,6 +36,9 @@ public class AllianceMemberAdapter extends RecyclerView.Adapter<AllianceMemberAd
     public void onBindViewHolder(@NonNull VH holder, int position) {
         User member = members.get(position);
         holder.tvName.setText(member.getUsername());
+
+        int avatarRes = getAvatarResource(member.getAvatar());
+        holder.imgAvatar.setImageResource(avatarRes);
     }
 
     @Override
@@ -43,11 +47,24 @@ public class AllianceMemberAdapter extends RecyclerView.Adapter<AllianceMemberAd
     }
 
     static class VH extends RecyclerView.ViewHolder {
+        ImageView imgAvatar;
         TextView tvName;
 
         VH(View v) {
             super(v);
+            imgAvatar = v.findViewById(R.id.imgAvatar);
             tvName = v.findViewById(R.id.tvMemberName);
+        }
+    }
+
+    private int getAvatarResource(int index) {
+        switch (index) {
+            case 1: return R.drawable.avatar1;
+            case 2: return R.drawable.avatar2;
+            case 3: return R.drawable.avatar3;
+            case 4: return R.drawable.avatar4;
+            case 5: return R.drawable.avatar5;
+            default: return R.drawable.avatar1;
         }
     }
 }
