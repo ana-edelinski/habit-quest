3b18ee2146da032aa3f0cc0e4853e032afe96ea1
Ana Edelinski <aedelinski85@gmail.com>
2025-10-06 17:08:17 +0200

    Delete unnecessary fragment

--- Habit-quest/app/src/main/java/com/example/habitquest/presentation/viewmodels/NotificationsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.habitquest.presentation.viewmodels;
-
-import androidx.lifecycle.ViewModel;
-
-public class NotificationsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
